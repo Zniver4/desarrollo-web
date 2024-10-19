9c0431481774aaa031c96086fb01d71fa6b44df7
Zniver4 <jonathan4zacos@gmail.com>
2024-10-19 14:43:20 -0600

    practica 8

--- docs/Practica-5/practica-5.js
+++ docs/Practica-5/practica-5.js
@@ -1,101 +1,87 @@
 let productos = [
-  { nombre: "Camiseta", precio: 15, stock: 10 },
-  { nombre: "Pantalones", precio: 25, stock: 8 },
-  { nombre: "Zapatos", precio: 50, stock: 5 },
-  { nombre: "Sombrero", precio: 10, stock: 20 },
+    { nombre: "Camiseta", precio: 15, stock: 10 },
+    { nombre: "Pantalones", precio: 25, stock: 8 },
+    { nombre: "Zapatos", precio: 50, stock: 5 },
+    { nombre: "Sombrero", precio: 10, stock: 20 },
 ];
-
 let carrito = [];
-
 function agregarAlCarrito(productoNombre, cantidad) {
-  for (let producto of productos) {
-      if (producto.nombre === productoNombre) {
-          if (producto.stock >= cantidad) {
-              carrito.push({
-                  nombre: productoNombre,
-                  cantidad: cantidad,
-                  precio: producto.precio,
-              });
-
-              producto.stock -= cantidad;
-              console.info(`${cantidad} ${productoNombre}(s) agregado(s) al carrito`);
-          } else {
-              console.error(`No hay suficiente stock de ${productoNombre}`);
-          }
-          return;
-      }
-  }
-  console.error(`El producto "${productoNombre}" no existe.`);
+    for (let producto of productos) {
+        if (producto.nombre === productoNombre) {
+            if (producto.stock >= cantidad) {
+                carrito.push({
+                    nombre: productoNombre,
+                    cantidad: cantidad,
+                    precio: producto.precio,
+                });
+                producto.stock -= cantidad;
+                console.info(`${cantidad} ${productoNombre}(s) agregado(s) al carrito`);
+            } else {
+                console.error(`No hay suficiente stock de ${productoNombre}`);
+            }
+            return;
+        }
+    }
+    console.error(`El producto "${productoNombre}" no existe.`);
 }
-
 function eliminarDelCarrito(productoNombre) {
-  for (let i = 0; i < carrito.length; i++) {
-      if (carrito[i].nombre === productoNombre) {
-          let cantidad = carrito[i].cantidad;
-          carrito.splice(i, 1);
-          for (let producto of productos) {
-              if (producto.nombre === productoNombre) {
-                  producto.stock += cantidad;
-                  break;
-              }
-          }
-          console.info(`${productoNombre} eliminado del carrito`);
-          return;
-      }
-  }
-  console.error(`El producto "${productoNombre}" no está en el carrito.`);
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].nombre === productoNombre) {
+            let cantidad = carrito[i].cantidad;
+            carrito.splice(i, 1);
+            for (let producto of productos) {
+                if (producto.nombre === productoNombre) {
+                    producto.stock += cantidad;
+                    break;
+                }
+            }
+            console.info(`${productoNombre} eliminado del carrito`);
+            return;
+        }
+    }
+    console.error(`El producto "${productoNombre}" no está en el carrito.`);
 }
-
 function calcularTotal() {
-  let total = 0;
-  for (let item of carrito) {
-      total += item.precio * item.cantidad;
-  }
-  return total;
+    let total = 0;
+    for (let item of carrito) {
+        total += item.precio * item.cantidad;
+    }
+    return total;
 }
-
 function aplicarDescuento(total) {
-  if (total > 100) {
-      return total * 0.9;
-  }
-  return total;
+    if (total > 100) {
+        return total * 0.9;
+    }
+    return total;
 }
-
 function mostrarTiempoRestante(segundos) {
-  let contador = segundos;
-
-  let intervalo = setInterval(function() {
-      if (contador > 0) {
-          console.log(`Compra confirmada en ${contador}...`);
-          contador--;
-      } else {
-          console.log("Compra confirmada.");
-          clearInterval(intervalo);
-      }
-  }, 1000);
+    let contador = segundos;
+    let intervalo = setInterval(function() {
+        if (contador > 0) {
+            console.log(`Compra confirmada en ${contador}...`);
+            contador--;
+        } else {
+            console.log("Compra confirmada.");
+            clearInterval(intervalo);
+        }
+    }, 1000);
 }
-
-// Ejemplo de uso
+  // Ejemplo de uso
 mostrarTiempoRestante(3);
-
 function procesarCompra() {
-  console.log("Procesando compra...");
-  mostrarTiempoRestante(3); // Llama a la función para mostrar el tiempo restante
-  setTimeout(function () {
-      let total = calcularTotal();
-      total = aplicarDescuento(total);
-      console.log(`Compra completada. Total a pagar: $${total.toFixed(2)}`);
-  }, 3000);
+    console.log("Procesando compra...");
+    mostrarTiempoRestante(3); // Llama a la función para mostrar el tiempo restante
+    setTimeout(function () {
+        let total = calcularTotal();
+        total = aplicarDescuento(total);
+        console.log(`Compra completada. Total a pagar: $${total.toFixed(2)}`);
+    }, 3000);
 }
-
-
 agregarAlCarrito("Pantalones", 3);
 agregarAlCarrito("Pantalones", 4);
 agregarAlCarrito("Zapatos", 2);
 agregarAlCarrito("Camiseta", 3);
 console.log(carrito);
-
 eliminarDelCarrito("Pantalones");
 console.log(carrito);
-
 procesarCompra();
---
+++ docs/Practica-8/cargaproduct.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const productContainer = document.getElementById('products');
+    productContainer.classList.add('row', 'justify-content-center');
+
+    const productsAPIFetch = fetch('https://fakestoreapi.com/products')
+        .then(response => response.json());
+
+    Promise.all([productsAPIFetch])
+    .then(([data]) => {
+        const productsAPI = data;
+
+        // Validador
+        if (!Array.isArray(productsAPI)) {
+            throw new Error("No es un array válido");
+        }
+
+        // DOM
+        productsAPI.forEach(product => {
+            const productDiv = document.createElement('div');
+            productDiv.classList.add('col-12', 'col-md-4', 'producto');
+            productDiv.innerHTML = `
+                <div class="box">
+                    <img src="${product.image}" class="card-img-top" alt="${product.title}" style="max-height: 200px; object-fit: contain;">
+                    <div class="card-body">
+                        <h5 class="card-title" style="font-size: 25px;" data-id="${product.id}" data-title="${product.title}">${product.title}</h5>
+                        <p class="card-text" style="font-size: 15px;">${product.description}</p>
+                        <p class="text-primary" style="font-size: 20px;" data-price="${product.price}">$${product.price}</p>
+                        <div class="botones, justify-content-center">
+                            <button class="btn-restar" disabled>-</button>
+                            <button class="btn-sumar">+</button>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+            productContainer.appendChild(productDiv);
+        });
+    })
+    .catch(error => console.error('Error en la carga del API', error));
+});
